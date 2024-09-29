a398a7716bca47fcc17c22b84e5900bdcdcbfa6a
PhyremDan <daniela@storsuite.com>
2024-09-30 06:03:32 +0800

    content update!

--- website/script.js
+++ website/script.js
@@ -1,18 +1,77 @@
+document.getElementById('feedbackForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    
+    const emailInput = document.getElementById('email');
+    const messageInput = document.getElementById('message');
+    
+    const email = emailInput.value;
+    const message = messageInput.value;
+
+    // Get reCAPTCHA token
+    grecaptcha.ready(async function() {
+        const recaptchaToken = await grecaptcha.execute('6LdtA1IqAAAAAGv9imy5hhgEt5_H5wx23WqE9EHS', {action: 'submit'});
+
+        try {
+            const response = await fetch('https://168ah2wg02.execute-api.us-east-1.amazonaws.com/prod/feedback', { 
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    message: message,
+                    recaptchaToken: recaptchaToken // Send reCAPTCHA token along with form data
+                })
+            });
+
+            // Check if the response is ok
+            if (!response.ok) {
+                throw new Error('Failed to send feedback');
+            }
+
+            // Parse the JSON response
+            const result = await response.json();
+            alert(result.message);  // Should display 'Feedback received successfully!'
+
+            // Clear the form fields after successful submission
+            emailInput.value = '';
+            messageInput.value = '';
+
+        } catch (error) {
+            alert(error.message);  // Display the error message
+        }
+    });
+});
+
+
+
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate');
+        } else {
+            sec.classList.remove('show-animate');
+        }
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sec1 = document.querySelector('.sec-1');
+    sec1.classList.add('show-animate');
+});
+
+
+
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://nspjqdglvc5d67edni3k432yfy0lyzso.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `This page has <b>${data}</b> views`;
+    counter.innerHTML = `<b>${data}</b> Page views`;
 }
-
 updateCounter();
-
-
-// Select the toggle switch input
-const toggleModeSwitch = document.getElementById('toggleModeSwitch');
-
-// Add change event listener to toggle switch
-toggleModeSwitch.addEventListener('change', function() {
-    // Toggle between light and dark mode
-    document.body.classList.toggle('dark-mode');
-});
